368fd327799233441d5ee91891c1bd1c5cd7ff71
Louveteau96 <114427182+Louveteau96@users.noreply.github.com>
2023-04-06 18:04:17 +0200

    Encore

--- src/villagegaulois/Etal.java
+++ src/villagegaulois/Etal.java
@@ -1,37 +1,70 @@
 package villagegaulois;
-import personnages.*;
+
+import personnages.Gaulois;
 import produit.*;
 
-public class Etal<P extends Produit>{
+public class Etal<P extends Produit> implements IEtal {
 	private Gaulois vendeur;
-	private P[] tabProduits;
-	
-	
-	@Override
-	public Gaulois getVendeur() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	@Override
-	public double donnerPrix() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-	@Override
-	public int contientProduit(String produit, int quantiteSouhaitee) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-	@Override
-	public double acheterProduit(int quantiteSouhaitee) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-	@Override
-	public String etatEtal() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
+	private IProduit[] tabProduits;
+	private int nbProduit;
+	private int prix;
 
+	
+	public void installerVendeur(Gaulois vendeur, IProduit[] produit, int prix){
+		this.vendeur = vendeur;
+		this.nbProduit = produit.length;
+		this.tabProduits = new IProduit[nbProduit];
+		for (int i = 0; i < nbProduit; i++) {
+			this.tabProduits[i]= produit[i];
+		}
+		this.prix = prix;
+	}
+	
+	public Gaulois getVendeur() {
+		return this.vendeur;
+	}
+
+	public double donnerPrix() {
+		return this.prix;
+	}
+
+	public int contientProduit(String produit, int quantiteSouhaitee) {
+		int quantiteAVendre = 0;
+		if (nbProduit != 0 && this.tabProduits[0].getNom().equals(produit)) {
+			if (nbProduit >= quantiteSouhaitee) {
+					quantiteAVendre = quantiteSouhaitee;
+			} else {
+					quantiteAVendre = nbProduit;
+			}
+		}
+		return quantiteAVendre;
+	}
+	
+	public double acheterProduit(int quantiteSouhaitee) {
+		double prixPaye = 0;
+		for (int i = nbProduit - 1; i > nbProduit - quantiteSouhaitee - 1 || i > 1; i--) {
+		prixPaye += tabProduits[i].calculerPrix(prix);
+		}
+		if (nbProduit >= quantiteSouhaitee) {
+		nbProduit -= quantiteSouhaitee;
+		} else {
+		nbProduit = 0;
+		}
+		return prixPaye;
+	}
+
+	public String etatEtal() {
+		StringBuilder chaine = new StringBuilder(vendeur.getNom());
+		if (nbProduit > 0) {
+			chaine.append(" vend ");
+			chaine.append(nbProduit + " produits :");
+			for (int i = 0; i < nbProduit; i++) {
+				chaine.append("\n- " + tabProduits[i].description());
+				}
+		} else {
+			chaine.append(" n'a plus rien à vendre.");
+		}
+		chaine.append("\n");
+		return chaine.toString();
+	}
 }
--- src/villagegaulois/IEtal.java
+++ src/villagegaulois/IEtal.java
@@ -1,10 +1,11 @@
 package villagegaulois;
-import personnages.*;
+
+import personnages.Gaulois;
 
 public interface IEtal {
 	Gaulois getVendeur();
 	double donnerPrix();
-	int contientProduit(String produit, int quantiteSouhaitee);
+	int contientProduit(String produit,int quantiteSouhaitee);
 	double acheterProduit(int quantiteSouhaitee);
 	String etatEtal();
 
